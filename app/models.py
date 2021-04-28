738bb64017fb44ce66449bce04db00bc2881b775
Tyler Wolfe-Adam <t.wolfeadam@gmail.com>
2021-04-28 13:18:04 -0500

    Start database creation (incomplete)

--- app/models.py
+++ app/models.py
@@ -1,11 +1,195 @@
-from app import db
+# coding: utf-8
+# Generated using sqlacodegen from Tyler's local postgresql db based on his proposed data dictionary
+from sqlalchemy import Boolean, CheckConstraint, Column, Date, Enum, ForeignKey, Integer, SmallInteger, String, Table, Text, UniqueConstraint, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
 
-# NOTE: Not currently used
-class User(db.Model):
-    uid = db.Column(db.Integer, primary_key=True)
-    account_type = db.Column(db.String(7))
-    email = db.Column(db.String(191), index=True, unique=True)
-    password_hash = db.Column(db.String(191))
+Base = declarative_base()
+metadata = Base.metadata
 
-    def __repr__(self):
-        return f"{self.account_type}User[{self.email}]"
+
+class Attitude(Base):
+    __tablename__ = 'attitude'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('attitude_seq'::regclass)"))
+    title = Column(String(191), nullable=False, unique=True)
+
+    seekers = relationship('Seekerprofile', secondary='seekerattitude')
+
+
+class Skill(Base):
+    __tablename__ = 'skill'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('skill_seq'::regclass)"))
+    title = Column(String(191), nullable=False, unique=True)
+    type = Column(Enum('tech', 'biz', name='skill_types'), nullable=False)
+
+
+class Useraccount(Base):
+    __tablename__ = 'useraccount'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('useraccount_seq'::regclass)"))
+    account_type = Column(Enum('Seeker', 'Company', 'Admin', name='account_types'), nullable=False)
+    email = Column(String(191), nullable=False, unique=True, comment='login email')
+    password = Column(String(191), nullable=False)
+
+
+t_companyprofile = Table(
+    'companyprofile', metadata,
+    Column('company_id', ForeignKey('useraccount.id'), nullable=False, unique=True),
+    Column('company_name', String(191), server_default=text("NULL::character varying")),
+    Column('city', String(191), server_default=text("NULL::character varying")),
+    Column('state_abbv', String(2), server_default=text("NULL::character varying")),
+    Column('zip_code', String(5), server_default=text("NULL::character varying")),
+    Column('website', String(191), server_default=text("NULL::character varying")),
+    CheckConstraint('company_id > 0')
+)
+
+
+t_seekerprofile = Table(
+    'seekerprofile', metadata,
+    Column('seeker_id', ForeignKey('useraccount.id'), nullable=False, unique=True),
+    Column('contact_email', String(191), nullable=False),
+    Column('first_name', String(191), server_default=text("NULL::character varying")),
+    Column('last_name', String(191), server_default=text("NULL::character varying")),
+    Column('contact_phone', Integer),
+    Column('city', String(191), server_default=text("NULL::character varying")),
+    Column('state_abbv', String(2), server_default=text("NULL::character varying")),
+    Column('zip_code', String(5), server_default=text("NULL::character varying")),
+    CheckConstraint('seeker_id > 0')
+)
+
+
+t_useractivity = Table(
+    'useractivity', metadata,
+    Column('user_id', ForeignKey('useraccount.id'), nullable=False, unique=True),
+    Column('is_active', Boolean, nullable=False, server_default=text("true")),
+    Column('join_date', TIMESTAMP(precision=0), nullable=False, server_default=text("now()")),
+    Column('last_login', TIMESTAMP(precision=0), nullable=False, server_default=text("now()")),
+    CheckConstraint('user_id > 0')
+)
+
+
+class Jobpost(Base):
+    __tablename__ = 'jobpost'
+    __table_args__ = (
+        CheckConstraint('company_id > 0'),
+        CheckConstraint('id > 0')
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('jobpost_seq'::regclass)"))
+    company_id = Column(ForeignKey('companyprofile.company_id'), nullable=False)
+    job_title = Column(String(191), nullable=False)
+    is_remote = Column(Boolean)
+    city = Column(String(191), server_default=text("NULL::character varying"))
+    state_abbv = Column(String(2), server_default=text("NULL::character varying"))
+    description = Column(Text)
+
+    company = relationship('Companyprofile')
+
+
+t_seekerattitude = Table(
+    'seekerattitude', metadata,
+    Column('seeker_id', ForeignKey('seekerprofile.seeker_id'), nullable=False),
+    Column('attitude_id', ForeignKey('attitude.id'), nullable=False),
+    CheckConstraint('attitude_id > 0'),
+    CheckConstraint('seeker_id > 0'),
+    UniqueConstraint('seeker_id', 'attitude_id')
+)
+
+
+class Seekerhistoryeducation(Base):
+    __tablename__ = 'seekerhistoryeducation'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+        CheckConstraint('seeker_id > 0')
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('seekerhistoryeducation_seq'::regclass)"))
+    seeker_id = Column(ForeignKey('seekerprofile.seeker_id'), nullable=False)
+    education_level = Column(String(191), nullable=False)
+    study_field = Column(String(191), nullable=False)
+    school = Column(String(191), server_default=text("NULL::character varying"))
+    city = Column(String(191), server_default=text("NULL::character varying"))
+    state_abbv = Column(String(2), server_default=text("NULL::character varying"))
+    active_enrollment = Column(SmallInteger)
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+    seeker = relationship('Seekerprofile')
+
+
+class Seekerhistoryjob(Base):
+    __tablename__ = 'seekerhistoryjob'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+        CheckConstraint('seeker_id > 0')
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('seekerhistoryjob_seq'::regclass)"))
+    seeker_id = Column(ForeignKey('seekerprofile.seeker_id'), nullable=False)
+    job_title = Column(String(191), nullable=False)
+    company = Column(String(191), server_default=text("NULL::character varying"))
+    city = Column(String(191), server_default=text("NULL::character varying"))
+    state_abbv = Column(String(2), server_default=text("NULL::character varying"))
+    active_employment = Column(SmallInteger)
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+    seeker = relationship('Seekerprofile')
+
+
+t_seekerskill = Table(
+    'seekerskill', metadata,
+    Column('seeker_id', ForeignKey('seekerprofile.seeker_id'), nullable=False),
+    Column('skill_id', ForeignKey('skill.id'), nullable=False),
+    Column('skill_level', Enum('1', '2', '3', '4', '5', name='skill_levels'), nullable=False, comment='1=familiar,5=expert'),
+    CheckConstraint('seeker_id > 0'),
+    CheckConstraint('skill_id > 0'),
+    UniqueConstraint('seeker_id', 'skill_id')
+)
+
+
+class Jobpostattitude(Base):
+    __tablename__ = 'jobpostattitude'
+    __table_args__ = (
+        CheckConstraint('attitude_id > 0'),
+        CheckConstraint('id > 0'),
+        CheckConstraint('jobpost_id > 0')
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('jobpostattitude_seq'::regclass)"))
+    jobpost_id = Column(ForeignKey('jobpost.id'), nullable=False)
+    attitude_id = Column(ForeignKey('attitude.id'), nullable=False)
+    importance_level = Column(Enum('1', '2', '3', '4', '5', '6', '7', name='importance_levels'), nullable=False, comment='1=required,4=preferred,7=optional')
+
+    attitude = relationship('Attitude')
+    jobpost = relationship('Jobpost')
+
+
+class Jobpostskill(Base):
+    __tablename__ = 'jobpostskill'
+    __table_args__ = (
+        CheckConstraint('id > 0'),
+        CheckConstraint('jobpost_id > 0'),
+        CheckConstraint('skill_id > 0')
+    )
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('jobpostskill_seq'::regclass)"))
+    jobpost_id = Column(ForeignKey('jobpost.id'), nullable=False)
+    skill_id = Column(ForeignKey('skill.id'), nullable=False)
+    skill_level_min = Column(Enum('1', '2', '3', '4', '5', name='skill_levels'), nullable=False, comment='1=familiar,5=expert')
+    importance_level = Column(Enum('1', '2', '3', '4', '5', '6', '7', name='importance_levels'), nullable=False, comment='1=required,4=preferred,7=optional')
+
+    jobpost = relationship('Jobpost')
+    skill = relationship('Skill')
--- config.py
+++ config.py
@@ -6,9 +6,31 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def build_db_url(user=None, pw=None, host=None, port=None, db=None):
+    # prioritize passed vars, then environment vars, then assume not included
+    user = user or os.environ.get('DATABASE_USERNAME') or ""
+    pw = pw or os.environ.get('DATABASE_PASSWORD') or ""
+    host = host or os.environ.get('DATABASE_HOST') or ""
+    port = port or os.environ.get('DATABASE_PORT') or ""
+    db = db or os.environ.get('DATABASE') or "mydb"
+
+    # based on: https://stackoverflow.com/a/20722229
+    url = "postgresql://"
+    if user or pw:
+        url += user
+        if pw:
+            url += f":{pw}@"
+    url += host  # if not provided, will just add empty string
+    if port:
+        url += f":{port}"
+    url += f"/{db}"
+    return url
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # Used by SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # When uploaded to heroku, DATABASE_URL will be set.
+    # For local testing, the fallback will be used (if listed)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or build_db_url()
     # When track is false, do not signal the application every time a change is about to be made in the database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
